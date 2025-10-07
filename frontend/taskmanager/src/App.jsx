ff330f0d08ea9f79cceca5cc477074ff0a08673b
Keerthi <rhkeerthi3@gmail.com>
2025-10-07 21:07:57 +0530

    Modified app.jsx

--- frontend/taskmanager/src/App.jsx
+++ frontend/taskmanager/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+import Dashboard from './pages/Admin/Dashboard';
+import ManageTasks from './pages/Admin/ManageTasks';
+import ManageUsers from './pages/Admin/ManageUsers';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+
+        <Routes>
+
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+
+        <Route element={<PrivateRoute allowedRoles={["admin"]}/>}/>
+          <Route path='/admin/dashboard' element={<Dashboard/>}/>
+          <Route path='/admin/tasks' element={<ManageTasks/>}/>
+          <Route path='/admin/create-tasks' element={<ManageUsers/>}/>
+         
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
