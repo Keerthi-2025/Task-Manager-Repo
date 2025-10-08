66f8ef3e4dc8ec24802ec2fdf7ad9499afc3995e
Keerthi <rhkeerthi3@gmail.com>
2025-10-08 15:43:23 +0530

    DB is connected

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -17,8 +18,13 @@ app.use(
 )
 
 
+//connect db
+connectDB();
+
+
 //Middleware
 app.use(express.json());
+
 
 //Routes
 // app.use("/api/auth", authRoutes);
