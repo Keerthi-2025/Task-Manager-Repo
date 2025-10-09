334b7d22322071f0784d3158cbcde8e30ffecf59
Keerthi <rhkeerthi3@gmail.com>
2025-10-09 16:16:18 +0530

    creating auth API for getUserInfo

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -8,11 +8,12 @@ const protect = async(req, res, next)=>{
 
         if(token && token.startsWith("Bearer")){
             token = token.split(" ")[1];  //extract token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            req.User = await User.findById(decoded.id).select("-password");
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)  //User must be user here 
+            req.user = await User.findById(decoded.id).select("-password");
             next();
         }else{
-            res.status(401).json({message: "Not authorozed, no token"});
+            res.status(401).json({message: "Not authorized, no token"});
         }
     }catch(error){
             res.status(401).json({message: "Token failed", error: error.message});
