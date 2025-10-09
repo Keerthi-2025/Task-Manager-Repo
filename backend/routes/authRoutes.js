8a4b1d15ed6ec4588e0507ff95928eb130cdade0
Keerthi <rhkeerthi3@gmail.com>
2025-10-09 20:16:16 +0530

    creating auth API for ImageUpload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,4 +12,12 @@ router.post("/login", loginUser);
 router.get("/profile",protect, getUserProfile);
 router.post("/profile", protect, updateUserProfile);
 
+router.post("/upload-image", upload.single("image"), (req,res)=>{
+    if(!req.file){
+        return res.status(404).json({message: "No file uploaded."});
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    res.status(200),express.json({imageUrl});
+})
+
 module.exports= router;
