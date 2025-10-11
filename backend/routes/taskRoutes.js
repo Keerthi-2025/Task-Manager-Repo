187c70bf847b240c89ed0d36d77e7b7a8d48256b
Keerthi <rhkeerthi3@gmail.com>
2025-10-11 20:37:38 +0530

    Created Task management API for delete task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -11,7 +11,8 @@ router.get("/", protect, getTasks);   //get all tasks, admin:all, user:assigned
 router.get("/:id", protect, getTasksById); //get task by ID;
 router.post("/", protect, adminOnly, createTask);   // create tasks, admin only
 router.put("/:id", protect, updateTask);   //update task details
-router.delete("/:id", adminOnly, deleteTask);  //delete task, admin only
+// router.delete("/:id", adminOnly, deleteTask);  //delete task, admin only
+router.delete("/:id", protect, adminOnly, deleteTask);
 router.put("/:id/status", protect,updateTaskStatus);   //update task status
 router.put("/:id/todo", protect, updateTaskChecklist); // update task checklist
 
