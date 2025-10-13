5138e7d09227186f11a901c69d894114a0f03472
Keerthi <rhkeerthi3@gmail.com>
2025-10-13 13:31:59 +0530

    Creating Report generation API for exportTasksReport

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -9,6 +9,54 @@ const excelJS = require("exceljs");
 //@acess  private  (Admin)
 const exportTasksReport = async(req,res) =>{
     try {
+        const tasks = await Task.find().populate("assignedTo", "name email")
+
+        const workbook = new excelJS.Workbook();
+        const worksheet = workbook.addWorksheet("Task Report");
+
+        worksheet.columns =[
+
+            {header: "Task ID", key: "_id", width:25},
+            {header: "Title", key: "title", width:30},
+            {header: "Task ID", key: "_id", width:25},
+            {header: "Description", key: "description", width:50},
+            {header: "Task ID", key: "_id", width:25},
+            {header: "Priority", key: "priority", width:20},
+            {header: "Status", key: "status", width:20},
+            {header: "Due Date", key: "dueDate", width:20},
+            {header: "Assigned To", key: "assignedTo", width:30},
+        ];
+
+        tasks.forEach((task) =>{
+            const assignedTo = task.assignedTo.map((user)=> `${user.name} (${user.email})`).join(",");
+            worksheet.addRow({
+                _id: task._id,
+                title:task.title,
+                description:task.description,
+                priority: task.priority,
+                status: task.status,
+                dueDate: task.dueDate.toISOString().split("T")[0],
+                assignedTo: assignedTo || "Unassigned"
+            });
+        });
+
+        res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
+        );
+
+        res.setHeader(
+             "Content-Disposition",
+        'attachment; filename="tasks_report.xlsx"'
+
+        );
+
+        return workbook.xlsx.write(res).then(()=>{
+            res.end();
+        });
+       
+            
+
         
     } catch (error) {
         
