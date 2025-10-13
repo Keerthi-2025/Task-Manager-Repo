783920ed32e32ae2fc5d08824a86c91deeb556fe
Keerthi <rhkeerthi3@gmail.com>
2025-10-13 13:49:20 +0530

    Creating Report generation API for exportUserReport

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -69,6 +69,76 @@ const exportTasksReport = async(req,res) =>{
 //@acess  private  (Admin)
 const exportUserReport = async(req,res) =>{
     try {
+
+        const users = await User.find().select("name email _id").lean();
+        const usertasks = await User.find().populate(
+            "assignedTo",
+            "name email _id"
+        );
+
+        const userTaskMap = {};
+        users.forEach((user)=>{
+            userTaskMap[user._id]={
+                name: user.name,
+                email: user.email,
+                taskCount:0,
+                pendingTasks:0,
+                inProgressTasks:0,
+                completedTasks:0
+            };
+        });
+
+        usertasks.forEach((task)=>{
+            if(task.assignedTo){
+                task.assignedTo.forEach((assignedTo) =>{
+                    if(userTaskMap[assignedUser._id]){
+                        userTaskMap[assignedUser._id].taskCount+=1;
+                        if(task.status === "Pending"){
+                            userTaskMap[assignedUser._id].pendingTasks +=1;
+                        }else if(task.status === "In Progress"){
+                            userTaskMap[assignedUser._id].inProgressTasks +=1;
+                        }else if(task.status === "Completed"){
+                            userTaskMap[assignedUser._id].completedTasks +=1;
+                        }
+                    }
+                });
+            }
+        });
+
+        
+          const workbook = new excelJS.Workbook();
+        const worksheet = workbook.addWorksheet(" User Task Report");
+
+           worksheet.columns =[
+
+            {header: "User Name", key: "name", width:30},
+            {header: "Email", key: "email", width:30},
+            {header: "Total Assigned Tasks", key: "taskCount", width:25},
+            {header: "Pending Tasks", key: "pendingTasks", width:50},
+            {header: "Completed Tasks", key: "completedTasks", width:50},
+            {header: "In Progress Tasks", key: "inProgressTasks", width:50},
+        ];
+
+        Object.values(userTaskMap).forEach((user)=>{
+            worksheet.addRow(user);
+        });
+
+         res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
+        );
+
+        res.setHeader(
+             "Content-Disposition",
+        'attachment; filename="users_report.xlsx"'
+        );
+
+        return workbook.xlsx.write(res).then(()=>{
+            res.end();
+        });
+
+
+        
         
     } catch (error) {
         
