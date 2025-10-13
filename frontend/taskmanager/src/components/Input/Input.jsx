f0a67e4efaeb7548b7a45a04c7ace39127e1bc19
Keerthi <rhkeerthi3@gmail.com>
2025-10-13 20:21:45 +0530

    added login to Login page

---
+++ frontend/taskmanager/src/components/Input/Input.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Inputs({type,placeholder,label,value,onChange}) {
+  return (
+    <div>Inputs</div>
+  )
+}
+
+export default Inputs
--- frontend/taskmanager/src/pages/Auth/Login.jsx
+++ frontend/taskmanager/src/pages/Auth/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Input/Input';
 
 function Login() {
+
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [error, seterror] = useState("");
+
+  const navigate = useNavigate();
+
+  //handle login  form submit
+  const handleLogin = async (e) =>{
+    e.preventDefault();
+  }
   return (
-    <AuthLayout>Login</AuthLayout>
+    <AuthLayout>
+      <div className='lg:w-[70] h-3/4 md:h-full  flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back  </h3>
+          <p className='text-xs mb-6'>Please enter ypur Login Details</p>
+
+
+          <form onSubmit={handleLogin}>
+            <Input
+            type='text'
+            placeholder='example@gmail.com'
+            label='Email address'
+            value={email}
+            onChange={({target})=>setemail(target.value)}/>
+          </form>
+      
+      </div>
+    </AuthLayout>
   )
 }
 
