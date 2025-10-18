3c4b21a3a4ff79aeb926c43c4995528ad04d2ad2
Keerthi <rhkeerthi3@gmail.com>
2025-10-18 20:44:38 +0530

    modified useUserrAuth file

--- frontend/taskmanager/src/hooks/useUserAuth.jsx
+++ frontend/taskmanager/src/hooks/useUserAuth.jsx
@@ -3,3 +3,17 @@ import {useNavigate} from "react-router-dom";
 import {UserContext} from "../components/context/useContext";
 
 
+export const useUserAuth = () =>{
+    const {user, loadimg, clearUser} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(loadimg) return;
+        if(user) return;
+
+        if(!user){
+            clearUser();
+            navigate("/login");
+        }
+    },  [user, loadimg, navigate]);
+};
