cbc08b9ceacfa6505bb5e67f1d97f7c496e70066
Keerthi <rhkeerthi3@gmail.com>
2025-10-24 13:01:27 +0530

    added function to TaskListTable file

--- frontend/taskmanager/src/components/layouts/TaskListTable.jsx
+++ frontend/taskmanager/src/components/layouts/TaskListTable.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 function TaskListTable({tableData}) {
+
+    const getStatusBadgeColor = (status) =>{
+        switch(status){
+            case 'Completed': return 'bg-green-100 text-green-500 border border-green-200';
+            case 'Pending': return 'bg-purple-500 text-purple-500 border border-purple-200';
+            case 'In progress': return 'bg-gray-100 text-gray-500 border border-gray-200';
+            default: return 'bg-gray-100 text-gray-500 border border-gray-200';
+        }
+    }
+
   return (
     <div>TaskListTable</div>
   )
