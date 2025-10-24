6fc788debc7b74361009471423a311d8df89d9a9
Keerthi <rhkeerthi3@gmail.com>
2025-10-24 13:04:18 +0530

    added function to TaskListTable file

--- frontend/taskmanager/src/components/layouts/TaskListTable.jsx
+++ frontend/taskmanager/src/components/layouts/TaskListTable.jsx
@@ -9,7 +9,19 @@ function TaskListTable({tableData}) {
             case 'In progress': return 'bg-gray-100 text-gray-500 border border-gray-200';
             default: return 'bg-gray-100 text-gray-500 border border-gray-200';
         }
+    };
+
+    const getPriorityBadgeColor = (priority) =>{
+        switch(priority){
+             case 'High': return 'bg-red-100 text-red-500 border border-red-200';
+            case 'Medium': return 'bg-orange-500 text-orange-500 border border-orange-200';
+            case 'Low': return 'bg-yellow-100 text-yellow-500 border border-yellow-200';
+            default: return 'bg-gray-100 text-gray-500 border border-gray-200';
+
+        }
     }
+
+
 
   return (
     <div>TaskListTable</div>
