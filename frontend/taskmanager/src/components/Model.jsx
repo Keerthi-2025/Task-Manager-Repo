71fe1bb87ad4d26bfa4efa1cd2729954e9c14951
Keerthi <rhkeerthi3@gmail.com>
2025-10-27 19:36:26 +0530

    created new file

---
+++ frontend/taskmanager/src/components/Model.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Model({children, isOpen, isClose, title, }) {
+    
+    if(!isOpen) return;
+
+    return null;
+    
+  return (
+    <div>Model</div>
+  )
+}
+
+export default Model
