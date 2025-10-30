2b46365f6342eddfdb5f412128390554ea30d5ee
Keerthi <rhkeerthi3@gmail.com>
2025-10-30 19:23:49 +0530

    modified file

--- frontend/taskmanager/src/components/Input/TaskStatusTabs.jsx
+++ frontend/taskmanager/src/components/Input/TaskStatusTabs.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-function TaskStatusTabs({ tabs,activeMenu,setActiveTab}) {
+function TaskStatusTabs({ tabs,activeMenu,activeTab}) {
   return (
-    <div>TaskStatusTabs</div>
+    <div>
+        <div>
+            {tabs.map((tab)=>{
+                <button 
+                key={tab.label}
+                className={`relative px-3 md:px-4 py-2 text-sm font-medium ${activeMenu === tab.label 
+                    ? 'text-blue-400' : 'text-gray-500 hover:text-gray-700'} cursor-pointer`}
+                    onClick={()=>setActiveTab(tab.label)}>
+                        <div>
+                            <span className='text-xs'>{tab.label}</span>
+                            <span 
+                             className={`text-xs px-3 md:px-4 py-2 ml-2 rounded-full font-medium ${activeTab === tab.label 
+                    ? 'bg-blue-400 text-black' : 'bg-gray-200 text-gray-600' }`}>
+                    {tab.count}</span>
+                        </div>
+
+                        {activeTab === tab.label && (
+                            <div className=''></div>
+                        )}
+                    </button>
+            })}
+        </div>
+    </div>
   )
 }
 
