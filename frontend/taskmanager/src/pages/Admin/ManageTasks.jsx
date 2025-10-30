bfa4c32142fdf81b98bb5049b2826ba0cf18881d
Keerthi <rhkeerthi3@gmail.com>
2025-10-30 16:36:30 +0530

    API Call to GetAllTasks

--- frontend/taskmanager/src/pages/Admin/ManageTasks.jsx
+++ frontend/taskmanager/src/pages/Admin/ManageTasks.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashBoardLayout from '../../components/layouts/DashBoardLayout'
+import { useNavigate } from 'react-router-dom';
 
 function ManageTasks() {
+
+  const [allTasks, setAllTasks]= useState([]);
+  const [tabs, setabs] = useState([]);
+  const [filtersStatus, setFiltersStatus] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getAllTasks = async()=>{
+
+  }
+
+  const handleClick = (taskData)=>{
+    navigate(`/admin/create-task`, {state:{taskId: taskData._id}})
+  }
+
+
+  //download task report
+  const handleDownloadReport = async () =>{};
+
+useEffect(() => {
+  getAllTasks(filtersStatus);
+
+  return () => {}
+  
+}, [filtersStatus]);
+
   return (
-    <div>ManageTasks</div>
+    
+    <DashBoardLayout activeMenu={ManageTasks}>
+      <div className='my-5'></div>
+    </DashBoardLayout>
   )
 }
 
