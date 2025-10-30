c8663bcc667632342db2b2677d7b2474cf78806d
Keerthi <rhkeerthi3@gmail.com>
2025-10-30 16:48:31 +0530

    API Call to GetAllTasks

--- frontend/taskmanager/src/pages/Admin/ManageTasks.jsx
+++ frontend/taskmanager/src/pages/Admin/ManageTasks.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import DashBoardLayout from '../../components/layouts/DashBoardLayout'
 import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPath';
 
 function ManageTasks() {
 
@@ -11,6 +13,29 @@ function ManageTasks() {
   const navigate = useNavigate();
 
   const getAllTasks = async()=>{
+
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS);
+      params:{
+        status: filtersStatus === "All" ? "" : filtersStatus
+      }
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks:[])
+
+      //map statussummary data with fixed labels and order
+      const statussummary = response.data?.statussummary || {};
+
+      const statusArray = [
+        {label:"All", count: statussummary.all || 0},
+        {label:"Pending", count:statussummary.pendingTasks || 0},
+        {label:"Completed", count:statussummary.CompletedTasks || 0},
+        {label:"In Progress", count:statussummary.inProgressTasks || 0},
+      ];
+
+      setabs(statusArray);
+      console.log("Error fetching users", error);
+    } catch (error) {
+      
+    }
 
   }
 
