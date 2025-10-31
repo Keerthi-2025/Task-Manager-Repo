e9b2cb806ad8a630cc685f1df3902577277ccb7c
Keerthi <rhkeerthi3@gmail.com>
2025-10-31 16:45:08 +0530

    calling Get All User Details API

--- frontend/taskmanager/src/pages/Admin/ManageUsers.jsx
+++ frontend/taskmanager/src/pages/Admin/ManageUsers.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashBoardLayout from '../../components/layouts/DashBoardLayout'
+import axios from 'axios';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPath';
 
 function ManageUsers() {
+
+  const[allUsers, setAllUsers] = useState([]);
+
+  const getAllUsers = async () =>{
+    try {
+      const response = await axiosInstance.get(API_PATHS.USERS.GET_ALL_USERS);
+      if(response.data?.length > 0){
+        setAllUsers(response.data);
+      }
+    } catch (error) {
+      
+      console.log("Error fetching users", error);
+      
+    }
+  };
+
+  //download task report
+  const handleDownloadReport = async () =>{}
+
+  useEffect(() => {
+    getAllUsers();
+  
+    return () => {}
+  }, [])
+  
   return (
-    <div>ManageUsers</div>
+    <DashBoardLayout activeMenu="Team Members">ManageUsers</DashBoardLayout>
   )
 }
 
