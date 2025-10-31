93616396fae7dbf4c35f2ef371881a6d5dc54717
Keerthi <rhkeerthi3@gmail.com>
2025-10-31 17:00:44 +0530

    calling Get All User Details API

--- frontend/taskmanager/src/pages/Admin/ManageUsers.jsx
+++ frontend/taskmanager/src/pages/Admin/ManageUsers.jsx
@@ -3,6 +3,7 @@ import DashBoardLayout from '../../components/layouts/DashBoardLayout'
 import axios from 'axios';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPath';
+import { LuFileSpreadsheet } from 'react-icons/lu';
 
 function ManageUsers() {
 
@@ -31,7 +32,22 @@ function ManageUsers() {
   }, [])
   
   return (
-    <DashBoardLayout activeMenu="Team Members">ManageUsers</DashBoardLayout>
+    <DashBoardLayout activeMenu="Team Members">
+      <div className='mt-5 mb-10'>
+        <div className='flex md:flex-row md:items-center justify-between'>
+          <h2 className='text-xl font-medium md:text-xl'>Team Members</h2>
+
+          <button className='flex md:flex download-btn' onClick={handleDownloadReport}>
+            <LuFileSpreadsheet className=' text-lg'/>Download Report</button>
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+          {allUsers?.map((user)=>{
+            <UserCard key={user._id} userInfo={user}/>
+          })}
+        </div>
+      </div>
+    </DashBoardLayout>
   )
 }
 
