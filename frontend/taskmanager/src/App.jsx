6aff8958e5b36be91b0589817a709a1f835924a3
Keerthi <rhkeerthi3@gmail.com>
2025-10-31 16:28:03 +0530

    modified files

--- frontend/taskmanager/src/App.jsx
+++ frontend/taskmanager/src/App.jsx
@@ -33,7 +33,10 @@ function App() {
         <Route element={<PrivateRoute allowedRoles={["admin"]}/>}>
           <Route path='/admin/dashboard' element={<Dashboard/>}/>
           <Route path='/admin/tasks' element={<ManageTasks/>}/>
-          <Route path='/admin/create-tasks' element={<CreateTask/>}/>
+
+         <Route path='/admin/create-task' element={<CreateTask />} />    {/* <Route path='/admin/create-task' element={<CreateTask/>}/> */}
+
+          
           <Route path='/admin/users' element={<ManageUsers/>}/>
         </Route>
 
--- frontend/taskmanager/src/pages/Admin/ManageTasks.jsx
+++ frontend/taskmanager/src/pages/Admin/ManageTasks.jsx
@@ -1,96 +1,4 @@
-// import React, { useEffect, useState } from 'react'
-// import DashBoardLayout from '../../components/layouts/DashBoardLayout'
-// import { useNavigate } from 'react-router-dom';
-// import axiosInstance from '../../utils/axiosInstance';
-// import { API_PATHS } from '../../utils/apiPath';
-// import { LuFileSpreadsheet } from 'react-icons/lu';
-// import TaskStatusTabs from '../../components/Input/TaskStatusTabs';
 
-// function ManageTasks() {
-
-//   const [allTasks, setAllTasks]= useState([]);
-//   const [tabs, setabs] = useState([]);
-//   const [filtersStatus, setFiltersStatus] = useState("All");
-
-//   const navigate = useNavigate();
-
-//   const getAllTasks = async()=>{
-
-//     try {
-//       const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS,{
-//         params:{
-//         status: filtersStatus === "All" ? "" : filtersStatus
-//       },
-//       });
-      
-//       setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks:[])
-
-//       //map statussummary data with fixed labels and order
-//       const statussummary = response.data?.statussummary || {};
-
-//       const statusArray = [
-//         {label:"All", count: statussummary.all || 0},
-//         {label:"Pending", count:statussummary.pendingTasks || 0},
-//         {label:"Completed", count:statussummary.CompletedTasks || 0},
-//         {label:"In Progress", count:statussummary.inProgressTasks || 0},
-//       ];
-
-//       setabs(statusArray);
-//       console.log("Error fetching users", error);
-//     } catch (error) {
-      
-//     }
-
-//   }
-
-//   const handleClick = (taskData)=>{
-//     navigate(`/admin/create-task`, {state:{taskId: taskData._id}})
-//   }
-
-
-//   //download task report
-//   const handleDownloadReport = async () =>{};
-
-// useEffect(() => {
-//   getAllTasks(filtersStatus);
-
-//   return () => {}
-  
-// }, [filtersStatus]);
-
-//   return (
-    
-//     <DashBoardLayout activeMenu={ManageTasks}>
-//       <div className='my-5'>
-//         <div className='flex flex-col lg:flex-row lg:items-center justify-between'>
-//           <div className=' flex items-center justify-between gap-3'>
-//             <h2 className='text-xl md:text-xl font-medium'>My Tasks</h2>
-
-//             <button
-//             className='flex lg:hidden donwload-btn'
-//             onClick={handleDownloadReport}>
-//               <LuFileSpreadsheet className='text-lg'/>Download Report</button>
-//           </div>
-
-//           {tabs?.[0].count > 0 &&(
-//             <div>
-//               <TaskStatusTabs
-//               tabs={tabs}
-//               activeMenu={filtersStatus}
-//               activeTab ={setFiltersStatus}/>
-
-//               <button className='hidden md:flex donwload-btn' onClick={handleDownloadReport}>
-//                 <LuFileSpreadsheet className='text-lg'/>Download Report</button>
-
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </DashBoardLayout>
-//   )
-// }
-
-// export default ManageTasks
 
 
 import React, { useEffect, useState } from 'react';
@@ -136,7 +44,7 @@ function ManageTasks() {
   };
 
   const handleClick = (taskData) => {
-    navigate(`/admin/create-task`, { state: { taskId: taskData._id } });
+navigate('/admin/create-task', { state: { taskId: taskData._id } });    
   };
 
   // download task report
